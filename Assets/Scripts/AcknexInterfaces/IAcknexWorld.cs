98ceaaa051a3671dadd2790998cde993cfdf52bd
Ricardo Reis <rickomax@hotmail.com>
2023-02-22 14:50:51 -0300

    Added Acknex.Interfaces.ObjectType enum and IAcknexWorld

---
+++ Assets/Scripts/AcknexInterfaces/IAcknexWorld.cs
@@ -0,0 +1,8 @@
+﻿namespace Acknex.Interfaces
+{
+    public interface IAcknexWorld
+    {
+        IAcknexObject CreateObject(ObjectType type, string name);
+        IAcknexObject GetObject(ObjectType type, string name);
+    }
+}
---
+++ Assets/Scripts/AcknexInterfaces/ObjectType.cs
@@ -0,0 +1,16 @@
+﻿namespace Acknex.Interfaces
+{
+    public enum ObjectType
+    {
+        Action,
+        Actor,
+        Bitmap,
+        Region,
+        Skill,
+        Synonym,
+        Texture,
+        Thing,
+        Wall,
+        Way
+    }
+}
