8d4f83578b2242d567f543bfd3f64e6dbf82a466
LPeter1997 <lenkefi.peti@gmail.com>
2023-02-27 14:43:59 +0100

    Update ScriptingEngine.cs

--- Assets/Scripts/WdlEngine/ScriptingEngine.cs
+++ Assets/Scripts/WdlEngine/ScriptingEngine.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Acknex.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -10,11 +11,18 @@ namespace WdlEngine
 {
     public sealed class ScriptingEngine
     {
+        private readonly IAcknexWorld _world;
+
+        public ScriptingEngine(IAcknexWorld world)
+        {
+            _world = world;
+        }
+
         public void Test(string rootPath, TextReader reader)
         {
             var lexedTokens = Lexer.Lex(reader);
             var preprocessedTokens = Preprocessor.Process(rootPath, lexedTokens);
-            WdlParser.Parse(null, preprocessedTokens);
+            WdlParser.Parse(_world, preprocessedTokens);
         }
     }
 }
